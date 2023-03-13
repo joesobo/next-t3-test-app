06493716982e932d8df8d2755709bd9f86ac03d4
joesobo <soboleskijoe@gmail.com>
2023-03-12 17:03:57 -0700

    shadcn ui components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
+import { Button } from "@/components/ui/button"
+import { CommandMenu } from "@/components/commandMenu"
+
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
@@ -20,6 +23,11 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
+
+					<Button variant="destructive">Button</Button>
+
+					<CommandMenu />
+
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
